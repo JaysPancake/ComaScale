a53e0d892c0b3d2b54f81033db92351790411af0
JaysPancake <jayden.s.bentley@gmail.com>
2023-11-06 22:38:34 -0600

    Attempting Copy Link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private snackBar: MatSnackBar, private router: Router) {}
   title = 'ComaScale';
 
   goToLink(url:string){
     window.open(url,"_blank");
+  }
+
+  copyUrl() {
+    // Create a dummy input to copy the string
+    let dummy = document.createElement('input');
+    // Get the text to copy
+    let text = window.location.href;
+    // Add the dummy input to the body
+    document.body.appendChild(dummy);
+    // Set the dummy input's value
+    dummy.value = text;
+    // Select the text
+    dummy.select();
+    // Copy the text
+    document.execCommand('copy');
+    // Remove the dummy input
+    document.body.removeChild(dummy);
+  
+    // Display the snackbar
+    this.snackBar.open('URL copied to clipboard', 'Close', {
+      duration: 3000
+    });
   }
 }
